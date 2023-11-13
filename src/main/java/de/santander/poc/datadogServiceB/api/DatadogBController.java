83601a2be0841f02ccd616947a2b75a2133a2edf
Inigo Moreno <inigomorenotato@gmail.com>
2023-11-13 10:39:39 +0100

    Datadog JSON log collection for service B

--- src/main/java/de/santander/poc/datadogServiceB/api/DatadogBController.java
+++ src/main/java/de/santander/poc/datadogServiceB/api/DatadogBController.java
@@ -1,6 +1,8 @@
 package de.santander.poc.datadogServiceB.api;
 
 import de.santander.poc.datadogServiceB.service.DatadogBService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/world")
 public class DatadogBController {
 
+    private static final Logger logger = LogManager.getLogger(DatadogBController.class);
     @Autowired
     private DatadogBService datadogBService;
 
     @GetMapping
     public ResponseEntity<String> getHelloWorldMessage() {
+        logger.info("Called by DatadogAService");
         return ResponseEntity.status(HttpStatus.OK).body(datadogBService.getDatadogBMessage());
     }
 }
--- src/main/java/de/santander/poc/datadogServiceB/service/DatadogBService.java
+++ src/main/java/de/santander/poc/datadogServiceB/service/DatadogBService.java
@@ -1,5 +1,7 @@
 package de.santander.poc.datadogServiceB.service;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -7,7 +9,10 @@ public class DatadogBService {
 
     private final String WORLD = "World";
 
+    private static final Logger logger = LogManager.getLogger(DatadogBService.class);
+
     public String getDatadogBMessage() {
+        logger.info("Returning info from DatadogBService");
         return WORLD;
     }
 }
